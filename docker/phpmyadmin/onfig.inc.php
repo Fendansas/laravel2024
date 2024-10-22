ddc03a9a093b4df3429b39ac7082960e30795ba3
sergey <fendansas@gmai.com>
2024-10-22 23:50:15 +0300

    first commit

---
+++ docker/phpmyadmin/onfig.inc.php
@@ -0,0 +1,5 @@
+<?php
+$cfg['Servers'][1]['host'] = 'mysql'; // имя сервиса контейнера MySQL
+$cfg['Servers'][1]['port'] = '3306';
+$cfg['Servers'][1]['user'] = 'root';
+$cfg['Servers'][1]['password'] = 'root';
