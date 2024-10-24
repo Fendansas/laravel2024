68ecdaf9d6079925d267e90e2ac22bce57f2a037
sergey <fendansas@gmai.com>
2024-10-24 17:33:34 +0300

    added laravel 11

--- src/resources/views/auth/register.blade.php
+++
@@ -1,7 +0,0 @@
-<form method="POST" action="{{ route('register') }}">
-    @csrf
-    <input type="text" name="name" placeholder="Name">
-    <input type="email" name="email" placeholder="Email">
-    <input type="password" name="password" placeholder="Password">
-    <button type="submit">Register</button>
-</form>
