d8423b25551a2a5622b6c33e500df5ca2657eeb4
sergey <fendansas@gmai.com>
2024-11-20 23:54:50 +0300

    added permission and role

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -16,6 +16,15 @@ return Application::configure(basePath: dirname(__DIR__))
             'is_admin' => AdminMiddleware::class
         ]);
     })
+
+    ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
